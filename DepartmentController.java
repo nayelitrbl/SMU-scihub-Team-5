1905351f2c49ae316b832709d61d1ef8ea105e95
nayelitrbl <nayelis0951@gmail.com>
2026-04-21 22:57:57 -0500

    Create new page /departments

---
+++ DepartmentController.java
@@ -0,0 +1,48 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.departments;
+
+
+import views.html.frequentlyAskedQuestions;
+import views.html.popularQueries;
+
+/**
+ * This controller handles department display information.
+ * Each menu item mainly renders a corresponding scala.html page.
+ */
+
+public class DepartmentController extends Controller {
+
+    /**
+     * This method renders departments.scala.html
+     * @return
+     */
+    public Result departmentList() {
+        return ok(departments.render());
+    }
+
+
+
+
+
+    /**
+     * This method renders frequentlyAskedQuestions.scala.html
+     * @return
+     */
+    public Result frequentlyAskedQuestions() {
+        return ok(frequentlyAskedQuestions.render());
+    }
+
+    /**
+     * This method renders popularQueries.scala.html
+     * @return
+     */
+    public Result popularQueries() {
+        return ok(popularQueries.render());
+    }
+
+
+
+}
